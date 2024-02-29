8c10a866b38304d02a5313f835b1e6f56010e677
mashhhok <ryabikovamari003@gmail.com>
2024-03-01 00:07:12 +0700

    improved validation

--- src/entities/User/schema.ts
+++ src/entities/User/schema.ts
@@ -2,10 +2,26 @@ import { z } from "zod";
 import { users } from "@/shared/database/schema";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
-export const insertUserValidator = createInsertSchema(users).omit({
-  id: true,
-  createdAt: true,
-});
+const nameValidator = z
+  .string()
+  .min(2)
+  .refine(
+    (value) => /^(?:[a-zA-Z]+|[а-яА-Я]+)$/.test(value),
+    "Should contain just one word"
+  );
+
+export const insertUserValidator = createInsertSchema(users)
+  .omit({
+    id: true,
+    createdAt: true,
+  })
+  .merge(
+    z.object({
+      email: z.string().email().toLowerCase(),
+      firstName: nameValidator,
+      lastName: nameValidator,
+    })
+  );
 
 export const selectUserValidator = createSelectSchema(users).omit({
   password: true,
