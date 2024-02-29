7729975f174170cc9057c6fbb709247bb1f043a8
mashhhok <ryabikovamari003@gmail.com>
2024-02-29 22:35:46 +0700

    user model

---
+++ src/shared/database/schema/users.ts
@@ -0,0 +1,11 @@
+import { mysqlTable, int, varchar, timestamp } from "drizzle-orm/mysql-core";
+
+export const users = mysqlTable("users", {
+  id: int("id").primaryKey().autoincrement(),
+  email: varchar("email", { length: 64 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
+  firstName: varchar("first_name", { length: 10 }).notNull(),
+  lastName: varchar("last_name", { length: 10 }).notNull(),
+  avatar: varchar("avatar", { length: 255 }),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
