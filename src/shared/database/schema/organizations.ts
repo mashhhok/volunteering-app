febbae47160c684bd32fda916eddaa3dd96ee7a6
mashhhok <ryabikovamari003@gmail.com>
2024-03-06 01:43:15 +0700

    required relation

--- src/shared/database/schema/organizations.ts
+++ src/shared/database/schema/organizations.ts
@@ -7,6 +7,7 @@ import { mysqlTable, varchar, int, timestamp } from "drizzle-orm/mysql-core";
 export const organizations = mysqlTable("organizations", {
   id: int("id").primaryKey().autoincrement(),
   userId: int("user_id")
+    .notNull()
     .unique()
     .references(() => users.id),
   companyName: varchar("company_name", { length: 20 }).notNull(),
