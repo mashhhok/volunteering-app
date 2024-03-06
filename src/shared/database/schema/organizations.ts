1a36cc8c2a1b9e4ee1ac1b5a0e1731ebb2c2ac91
mashhhok <ryabikovamari003@gmail.com>
2024-03-06 15:37:19 +0700

    description field

--- src/shared/database/schema/organizations.ts
+++ src/shared/database/schema/organizations.ts
@@ -2,12 +2,19 @@ import { users } from "./users";
 import { donationRequests } from "./donationRequests";
 
 import { relations } from "drizzle-orm";
-import { mysqlTable, varchar, int, timestamp } from "drizzle-orm/mysql-core";
+import {
+  mysqlTable,
+  varchar,
+  int,
+  timestamp,
+  text,
+} from "drizzle-orm/mysql-core";
 
 export const organizations = mysqlTable("organizations", {
   id: int("id").primaryKey().autoincrement(),
   userId: int("user_id").unique().notNull(),
   companyName: varchar("company_name", { length: 20 }).notNull(),
+  description: text("description").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
