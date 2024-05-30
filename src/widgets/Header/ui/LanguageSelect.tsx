985f28a14baa7fc86dfc82d3f3374678dd217c11
mashhhok <ryabikovamari003@gmail.com>
2024-05-30 17:24:57 +0300

    lang bug prefetch

--- src/widgets/Header/ui/LanguageSelect.tsx
+++ src/widgets/Header/ui/LanguageSelect.tsx
@@ -1,35 +1,26 @@
 "use client";
 import { IDictionary } from "@/shared/config/i18n.config";
 import { Select } from "@mantine/core";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export const LanguageSelect = ({ dict }: { dict: IDictionary }) => {
-  const [language, setLanguage] = React.useState<string | null>("en");
+  const [language, setLanguage] = React.useState<string | null>("lang");
   const firstRender = React.useRef<boolean>(true);
-
-  React.useEffect(() => {
-    const lang = localStorage.getItem("language");
-    setLanguage(lang ? lang : "en");
-  }, []);
-
-  React.useEffect(() => {
-    if (firstRender.current) {
-      firstRender.current = false;
-      return;
-    }
-    localStorage.setItem("language", language ? language : "en");
-  }, [language]);
-
+  const router = useRouter()
   return (
     <Select
       wrapperProps={{ width: "auto" }}
       w={"90px"}
       data={[
-        { label: dict.widgets.header.language_select.uk, value: "ua" },
-        { label: dict.widgets.header.language_select.en, value: "en" },
+        { label: "lang", value: "lang" },
+        { label: dict.widgets.header.language_select.uk, value: "uk" },
+        { label: dict.widgets.header.language_select.en, value: "en-US" },
       ]}
       value={language}
-      onChange={(value, option) => setLanguage(option.value)}
+      onChange={(value, option) => {
+        router.push(`/${option.value}`);
+      }}
     />
   );
 };
